b46639f38f6d2f6e8dc251b898b2261213b77c0a
weigq <42716996@qq.com>
2017-05-04 07:42:31 +0800

    网站和AS的冲突

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -9,15 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 这行不冲突
+
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"hello",Toast.LENGTH_SHORT).show();
 
-        // 我在v1.0分支上加了这条。我就是要跟v1.1冲突，就改这行
-        // q我也添加这行，怎么着吧
-        // 靠靠靠！！
-
-        //再加点内容吧。
-
+        Toast.makeText(this,"hello2",Toast.LENGTH_SHORT).show();
     }
 }
